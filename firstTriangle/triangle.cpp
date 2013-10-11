19404a98fa8290de276cd4826db905b5148a0cc0
kerimc <mirek.chomicz@gmail.com>
2013-10-11 23:11:37 +0200

    shader compile logs,

--- firstTriangle/triangle.cpp
+++ firstTriangle/triangle.cpp
@@ -18,33 +18,50 @@ int init_resources()
 #ifdef GL_ES_VERSION_2_0
 		"#version 100 \n"
 #else
-		"#version 120 \n"
+		"#version 130 \n"
 #endif
-		"attribute vec2 coord2d; \n"
+	"attribute vec2 coord2d; \n"
+	    "in vec4 position; \n"
+		"out vec4 color; \n"
 		"void main() { \n"
-		" gl_Position = vec4(coord2d, 0.0, 1.0); \n"
-		"}";
+	" gl_Position = vec4(coord2d, 0.0, 1.0); \n"
+	" color = vec4(coord2d, 0.0, 1.0);"
+	//	"gl_Position = position; \n"
+		"} \n";
 	glShaderSource(vs, 1, &vsSource, NULL);
 	glCompileShader(vs);
 	glGetShaderiv(vs, GL_COMPILE_STATUS, &compileOk);
 	if ( 0 == compileOk)
 	{
-		fprintf(stderr,"Error in vertex shader \n");
+		char log[300];
+		GLsizeiptr logLen;
+		
+		glGetShaderInfoLog(vs,300,&logLen, log); 
+		fprintf(stderr, vsSource);
+		fprintf(stderr, log);
+		
 		return 0;
 	}
 	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
 	const char * fsSource =
-    "#version 120           \n"
-    "void main(void) {        "
-    "  gl_FragColor[0] = 0.0; "
-    "  gl_FragColor[1] = 0.0; "
-    "  gl_FragColor[2] = 1.0; "
+    "#version 130           \n"
+	" in vec4 color; \n"
+    "void main(void) {       \n "
+ //   "  gl_FragColor = color; \n"
+" gl_FragColor[0] = gl_FragCoord.x/640.0; \n"
+"gl_FragColor[1] = gl_FragCoord.y/480.0; \n"
+"gl_FragColor[2] = 0.5; \n"
     "}";
   glShaderSource(fs, 1, &fsSource, NULL);
   glCompileShader(fs);
   glGetShaderiv(fs, GL_COMPILE_STATUS, &compileOk);
   if (!compileOk) {
-    fprintf(stderr, "Error in fragment shader\n");
+   char log[300];
+		GLsizeiptr logLen;
+		
+		glGetShaderInfoLog(fs,300,&logLen, log); 
+		fprintf(stderr, fsSource);
+		fprintf(stderr, log);
     return 0;
   }
   program = glCreateProgram();
@@ -77,23 +94,22 @@ void onDisplay()
      0.0,  0.8,
     -0.8, -0.8,
      0.8, -0.8,
+
   };
   /* Describe our vertices array to OpenGL (it can't guess its format automatically) */
   glVertexAttribPointer(
     attribute_coord2d, // attribute
-    2,                 // number of elements per vertex, here (x,y)
+    2,                 // number of elements per vertex, here (x,y, z)
     GL_FLOAT,          // the type of each element
     GL_FALSE,          // take our values as-is
     0,                 // no extra data between each position
     triangle_vertices  // pointer to the C array
-  );
- 
-  /* Push each element in buffer_vertices to the vertex shader */
+ );
+   /* Push each element in buffer_vertices to the vertex shader */
   glDrawArrays(GL_TRIANGLES, 0, 3);
   glDisableVertexAttribArray(attribute_coord2d);
- 
-  /* Display the result */
-  glutSwapBuffers();
+
+   glutSwapBuffers();
 }
 void free_resources()
 {
